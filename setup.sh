2fd3bb6ba8d1ab4ce1bfff481212fe6e5989460b
evengers <steven.rogers@evengers.com>
2013-07-20 10:47:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,16 +8,19 @@ sudo add-apt-repository -y ppa:mapnik/boost
 sudo apt-get update
 sudo apt-get install -y git build-essential postgresql-9.1-postgis postgresql-server-dev-9.1 libmapnik libmapnik-dev mapnik-utils python-mapnik libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-python-dev libboost-regex-dev libboost-system-dev libboost-thread-dev redis-server imagemagick gdal-bin
 
+
+#follow these instructions instead to configure nose with npm
+#https://www.digitalocean.com/community/articles/how-to-install-an-upstream-version-of-node-js-on-ubuntu-12-04
 #installing node
-cd ~
-mkdir src
-cd src
-wget http://nodejs.org/dist/v0.8.9/node-v0.8.9.tar.gz
-tar xzfv node-v0.8.9.tar.gz
-cd node-v0.8.9
-./configure
-make
-sudo make install
+#cd  ~
+#mkdir src
+# cd src
+#wget http://nodejs.org/dist/v0.8.9/node-v0.8.9.tar.gz
+#tar xzfv node-v0.8.9.tar.gz
+#cd node-v0.8.9
+#./configure
+#make
+#sudo make install
 
 
 #configuring postgis
